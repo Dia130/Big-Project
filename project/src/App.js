7ee5c0afae85c999c2fd4931c94186ac72a0a83b
Dia130 <garaevanigina@gmail.com>
2023-04-14 20:28:53 +0500

    Added react-app

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('')
   return (
-    <div>Hello</div>
+    <div>
+      <h1>{count}</h1>
+      <h1>{count >= 10 ? "Yes" : 'No'}</h1>
+      <button onClick={() => { setCount(count + 1) }}>+</button>
+      <button onClick={() => { setCount(count - 1) }}>-</button>
+    
+      <h1>{value}</h1>
+      <input type='text' value={value} onChange={(event)=>setValue(event.target.value)} />
+    </div>
   );
 }
 
